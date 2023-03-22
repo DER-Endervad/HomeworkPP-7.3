f1f0aa0bd2a8d7ac8ea17e80c94e412c32fda9cc
Даниил <derendervad@gmail.com>
2023-03-22 22:19:33 +0300

    initial commit

---
+++ HomeworkPP 7.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "S_vector.h"
+#include <Windows.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    S_vector<int> vector(5);
+    std::cout << "4 ячейка до:" << vector.at(3) << std::endl;
+    vector.at(3) = 4;
+    std::cout << "4 ячейка после:" << vector.at(3) << std::endl;
+    std::cout << "Запасное место после создания: " << vector.capacity() << std::endl;
+    vector.push_back(6);
+    std::cout << "6 ячейка:" << vector.at(5) << std::endl;
+    std::cout << "Запасное место после добавления элемента: " << vector.capacity() << std::endl;
+    vector.push_back(7);
+    std::cout << "7 ячейка:" << vector.at(6) << std::endl;
+    std::cout << "Запасное место после добавления элемента: " << vector.capacity() << std::endl;
+}
---
+++ S_vector.h
@@ -0,0 +1,61 @@
+#pragma once
+template<class T>
+class S_vector
+{
+private:
+	T* arr;
+	int s_size = 0;
+	int s_full = 0;
+	
+public:
+	S_vector(int size) {
+		arr = new T[size];
+		s_size = size;
+		s_full = size;
+	}
+
+	~S_vector<T>() {
+		delete arr;
+	}
+
+	S_vector& operator=(const S_vector& copy) {
+		this->arr = copy.arr;
+		delete copy.arr;
+		this->s_size = copy.s_size;
+		this->s_full = copy.s_full;
+	}
+
+	S_vector(const S_vector& copy) {
+		this->arr = copy.arr;
+		this->s_size = copy.s_size;
+		this->s_full = copy.s_full;
+	}
+
+	T& at(const int& number) {
+		return arr[number];
+	}
+
+	void push_back(T value) {
+		if (s_size == s_full) {
+
+			s_full = s_size + (s_size / 2);
+			T* arr2 = new int[s_full];
+			for (int i = 0; i < s_size; ++i) {
+				arr2[i] = arr[i];
+			}
+			arr2[s_size] = value;
+			++s_size;
+			delete[] arr;
+			arr = arr2;
+		}
+		else {
+			arr[s_size] = value;
+			++s_size;
+		}
+	}
+
+	int capacity() {
+		return s_full - s_size;
+	}
+};
+
